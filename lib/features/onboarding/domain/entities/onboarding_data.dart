834f994eca0e9324b5dcda223eac6014392f5dab
TarunTalan <taruntalan12@gmail.com>
2026-07-04 02:45:08 +0530

    feat(onboarding): implement onboarding data store and entities

---
+++ lib/features/onboarding/domain/entities/onboarding_data.dart
@@ -0,0 +1,107 @@
+import 'package:equatable/equatable.dart';
+
+class OnboardingData extends Equatable {
+  final String fullName;
+  final String dob;
+  final String gender;
+  final String weight;
+  final String weightUnit;
+  final String height;
+  final String heightUnit;
+  final String oxygenLevel;
+  final String healthConditions;
+  final String allergies;
+  final String medicines;
+  final String smokes;
+  final String bloodPressureTop;
+  final String bloodPressureBottom;
+  final String bpm;
+  final String activity;
+  final String sleep;
+  final String currentStep;
+
+  const OnboardingData({
+    this.fullName = "",
+    this.dob = "",
+    this.gender = "",
+    this.weight = "70",
+    this.weightUnit = "kg",
+    this.height = "",
+    this.heightUnit = "cm",
+    this.oxygenLevel = "",
+    this.healthConditions = "",
+    this.allergies = "",
+    this.medicines = "",
+    this.smokes = "",
+    this.bloodPressureTop = "",
+    this.bloodPressureBottom = "",
+    this.bpm = "",
+    this.activity = "",
+    this.sleep = "",
+    this.currentStep = "loading",
+  });
+
+  OnboardingData copyWith({
+    String? fullName,
+    String? dob,
+    String? gender,
+    String? weight,
+    String? weightUnit,
+    String? height,
+    String? heightUnit,
+    String? oxygenLevel,
+    String? healthConditions,
+    String? allergies,
+    String? medicines,
+    String? smokes,
+    String? bloodPressureTop,
+    String? bloodPressureBottom,
+    String? bpm,
+    String? activity,
+    String? sleep,
+    String? currentStep,
+  }) {
+    return OnboardingData(
+      fullName: fullName ?? this.fullName,
+      dob: dob ?? this.dob,
+      gender: gender ?? this.gender,
+      weight: weight ?? this.weight,
+      weightUnit: weightUnit ?? this.weightUnit,
+      height: height ?? this.height,
+      heightUnit: heightUnit ?? this.heightUnit,
+      oxygenLevel: oxygenLevel ?? this.oxygenLevel,
+      healthConditions: healthConditions ?? this.healthConditions,
+      allergies: allergies ?? this.allergies,
+      medicines: medicines ?? this.medicines,
+      smokes: smokes ?? this.smokes,
+      bloodPressureTop: bloodPressureTop ?? this.bloodPressureTop,
+      bloodPressureBottom: bloodPressureBottom ?? this.bloodPressureBottom,
+      bpm: bpm ?? this.bpm,
+      activity: activity ?? this.activity,
+      sleep: sleep ?? this.sleep,
+      currentStep: currentStep ?? this.currentStep,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        fullName,
+        dob,
+        gender,
+        weight,
+        weightUnit,
+        height,
+        heightUnit,
+        oxygenLevel,
+        healthConditions,
+        allergies,
+        medicines,
+        smokes,
+        bloodPressureTop,
+        bloodPressureBottom,
+        bpm,
+        activity,
+        sleep,
+        currentStep,
+      ];
+}
